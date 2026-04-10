3e476072d5511eb7f5d7d980a06b87100162a085
Florian Golubic <florian.golubic@gmail.com>
2026-04-10 23:46:54 +0200

    Update Program.cs

--- MetroNetApp/Program.cs
+++ MetroNetApp/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace MetroNetApp;
+﻿using MetroNetApp.classes;
+
+namespace MetroNetApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CityNetwork opticFiberNet = new();
+
+        // 1. Add Buildings
+        opticFiberNet.AddBuilding("City Hall");
+        opticFiberNet.AddBuilding("Hospital");
+        opticFiberNet.AddBuilding("Library");
+
+        // 2. Add Potential Routes
+        opticFiberNet.AddTrenchRoute("City Hall", "Hospital", 1200);
+        opticFiberNet.AddTrenchRoute("City Hall", "Library", 500);
+        opticFiberNet.AddTrenchRoute("Library", "Hospital", 900);
+
+        // 3. Run algorithm
+        List<TrenchRoute> finalPlan = opticFiberNet.CalculateOptimalNetwork();
+
+        // 4. Output results
+        Console.WriteLine("=== METRO-NET FIBER OPTIC OPTIMAL PLAN ===");
+        float totalCable = 0;
+        foreach (var f in finalPlan)
+        {
+            Console.WriteLine($"Link: {f.Source.Name} <-> {f.Target.Name} | Distance: {f.Distance}m");
+            totalCable += f.Distance;
+        }
+        Console.WriteLine("==========================================");
+        Console.WriteLine($"Total Cable Required: {totalCable / 1000} km");
     }
 }
