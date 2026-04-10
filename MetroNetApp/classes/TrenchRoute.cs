a27f892f610777304021f48e8008d520e26700ab
Florian Golubic <florian.golubic@gmail.com>
2026-04-10 23:46:50 +0200

    Update TrenchRoute.cs

--- MetroNetApp/classes/TrenchRoute.cs
+++ MetroNetApp/classes/TrenchRoute.cs
@@ -2,7 +2,7 @@ using DataStructureLibrary.Graph;
 
 namespace MetroNetApp.classes;
 
-public class TrenchRoute(uint id, Vertex? source, Vertex? target, float distance): Edge(id, source, target), IComparable<TrenchRoute>
+public class TrenchRoute(uint id, Vertex source, Vertex target, float distance): Edge(id, source, target), IComparable<TrenchRoute>
 {
     // Fields
     public float Distance = distance; // Physical distance of the trench == weight of the edge
@@ -12,13 +12,13 @@ public class TrenchRoute(uint id, Vertex? source, Vertex? target, float distance
     /// </summary>
     /// <param name="other"></param>
     /// <returns>Integer</returns>
-    public int CompareTo(TrenchRoute other)
+    public int CompareTo(TrenchRoute? other)
     {
         /*
             < 0 => this route is shorter than the other
             0   => Same distance
             > 0 => this route is longer than the other
         */
-        return Distance.CompareTo(other.Distance);
+        return Distance.CompareTo(other?.Distance);
     }
 }
